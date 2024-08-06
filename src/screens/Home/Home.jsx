b0e3012e4d5ca47dd8c3bfbc3489a0f62413418d
sunfe2009 <sunfe2009@qq.com>
2024-08-06 19:42:36 +0800

    1

---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,359 @@
+import React from "react";
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="div">
+        <div className="thumbnail">
+          <div className="text-wrapper">Enterprise - 2019</div>
+          <div className="text-wrapper-2">Project Name</div>
+          <div className="overlap">
+            <div className="safari">
+              <div className="overlap-group">
+                <img className="BG" alt="Bg" src="https://c.animaapp.com/GMqBQNDG/img/bg-10.svg" />
+                <div className="interface">
+                  <div className="adress">
+                    <div className="overlap-group-2">
+                      <img className="page" alt="Page" src="https://c.animaapp.com/GMqBQNDG/img/page-1-2.svg" />
+                      <div className="domain">website.co</div>
+                    </div>
+                  </div>
+                  <div className="new-page">
+                    <img className="icon" alt="Icon" src="https://c.animaapp.com/GMqBQNDG/img/icon-4.svg" />
+                  </div>
+                  <div className="sites">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-3">
+                        <img
+                          className="combined-shape"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-26.svg"
+                        />
+                        <img
+                          className="img"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-27.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="share">
+                    <div className="div-wrapper">
+                      <div className="overlap-group-4">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                      </div>
+                    </div>
+                  </div>
+                  <img className="dots" alt="Dots" src="https://c.animaapp.com/GMqBQNDG/img/dots-5@2x.png" />
+                  <img className="arrows" alt="Arrows" src="https://c.animaapp.com/GMqBQNDG/img/arrows-5@2x.png" />
+                  <div className="tabs">
+                    <img
+                      className="combined-shape-2"
+                      alt="Combined shape"
+                      src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-31.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail-2">
+          <div className="text-wrapper">Enterprise - 2019</div>
+          <div className="text-wrapper-2">Project Name</div>
+          <div className="overlap">
+            <div className="safari">
+              <div className="overlap-group">
+                <img className="BG" alt="Bg" src="https://c.animaapp.com/GMqBQNDG/img/bg-10.svg" />
+                <div className="interface">
+                  <div className="adress">
+                    <div className="overlap-group-5">
+                      <img className="page" alt="Page" src="https://c.animaapp.com/GMqBQNDG/img/page-1-4.svg" />
+                      <div className="domain">website.co</div>
+                    </div>
+                  </div>
+                  <div className="new-page">
+                    <img className="icon" alt="Icon" src="https://c.animaapp.com/GMqBQNDG/img/icon-4.svg" />
+                  </div>
+                  <div className="icon-wrapper">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-3">
+                        <img
+                          className="combined-shape"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-42.svg"
+                        />
+                        <img
+                          className="img"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-43.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="share-2">
+                    <div className="div-wrapper">
+                      <div className="overlap-group-4">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                      </div>
+                    </div>
+                  </div>
+                  <img className="dots" alt="Dots" src="https://c.animaapp.com/GMqBQNDG/img/dots-5@2x.png" />
+                  <img className="arrows" alt="Arrows" src="https://c.animaapp.com/GMqBQNDG/img/arrows-5@2x.png" />
+                  <div className="combined-shape-wrapper">
+                    <img
+                      className="combined-shape-2"
+                      alt="Combined shape"
+                      src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-47.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail-3">
+          <div className="text-wrapper">Enterprise - 2019</div>
+          <div className="text-wrapper-3">Project Name</div>
+          <div className="safari-wrapper">
+            <div className="safari">
+              <div className="overlap-group">
+                <img className="BG" alt="Bg" src="https://c.animaapp.com/GMqBQNDG/img/bg-4.svg" />
+                <div className="interface-2">
+                  <div className="adress">
+                    <div className="overlap-group-6">
+                      <img className="page" alt="Page" src="https://c.animaapp.com/GMqBQNDG/img/page-1-2.svg" />
+                      <div className="domain">website.co</div>
+                    </div>
+                  </div>
+                  <div className="new-page">
+                    <img className="icon" alt="Icon" src="https://c.animaapp.com/GMqBQNDG/img/icon-2.svg" />
+                  </div>
+                  <div className="sites-2">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-3">
+                        <img
+                          className="combined-shape"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-18.svg"
+                        />
+                        <img
+                          className="img"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-19.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="share-3">
+                    <div className="icon-2">
+                      <div className="overlap-group-4">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                      </div>
+                    </div>
+                  </div>
+                  <img className="dots" alt="Dots" src="https://c.animaapp.com/GMqBQNDG/img/dots-2@2x.png" />
+                  <img className="arrows" alt="Arrows" src="https://c.animaapp.com/GMqBQNDG/img/arrows-5@2x.png" />
+                  <div className="img-wrapper">
+                    <img
+                      className="combined-shape-2"
+                      alt="Combined shape"
+                      src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-23.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail-4">
+          <div className="text-wrapper">Enterprise - 2019</div>
+          <div className="text-wrapper-2">Project Name</div>
+          <div className="overlap-2">
+            <div className="safari">
+              <div className="overlap-group">
+                <img className="BG" alt="Bg" src="https://c.animaapp.com/GMqBQNDG/img/bg-10.svg" />
+                <div className="interface">
+                  <div className="adress">
+                    <div className="overlap-group-7">
+                      <img className="page" alt="Page" src="https://c.animaapp.com/GMqBQNDG/img/page-1-3.svg" />
+                      <div className="domain">website.co</div>
+                    </div>
+                  </div>
+                  <div className="new-page">
+                    <img className="icon" alt="Icon" src="https://c.animaapp.com/GMqBQNDG/img/icon-5.svg" />
+                  </div>
+                  <div className="sites-3">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-3">
+                        <img
+                          className="combined-shape"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-26.svg"
+                        />
+                        <img
+                          className="img"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-27.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="share-4">
+                    <div className="div-wrapper">
+                      <div className="overlap-group-4">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                      </div>
+                    </div>
+                  </div>
+                  <img className="dots" alt="Dots" src="https://c.animaapp.com/GMqBQNDG/img/dots-5@2x.png" />
+                  <img className="arrows" alt="Arrows" src="https://c.animaapp.com/GMqBQNDG/img/arrows-5@2x.png" />
+                  <div className="tabs-2">
+                    <img
+                      className="combined-shape-2"
+                      alt="Combined shape"
+                      src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-31.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail-5">
+          <div className="text-wrapper">Enterprise - 2019</div>
+          <div className="text-wrapper-2">Project Name</div>
+          <div className="overlap-3">
+            <div className="safari">
+              <div className="overlap-group">
+                <img className="BG" alt="Bg" src="https://c.animaapp.com/GMqBQNDG/img/bg-10.svg" />
+                <div className="interface">
+                  <div className="adress">
+                    <div className="overlap-group-8">
+                      <img className="page" alt="Page" src="https://c.animaapp.com/GMqBQNDG/img/page-1-4.svg" />
+                      <div className="domain">website.co</div>
+                    </div>
+                  </div>
+                  <div className="new-page">
+                    <img className="icon" alt="Icon" src="https://c.animaapp.com/GMqBQNDG/img/icon-4.svg" />
+                  </div>
+                  <div className="sites-4">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-3">
+                        <img
+                          className="combined-shape"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-42.svg"
+                        />
+                        <img
+                          className="img"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-43.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="share-5">
+                    <div className="div-wrapper">
+                      <div className="overlap-group-4">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                      </div>
+                    </div>
+                  </div>
+                  <img className="dots" alt="Dots" src="https://c.animaapp.com/GMqBQNDG/img/dots-5@2x.png" />
+                  <img className="arrows" alt="Arrows" src="https://c.animaapp.com/GMqBQNDG/img/arrows-5@2x.png" />
+                  <div className="tabs-3">
+                    <img
+                      className="combined-shape-2"
+                      alt="Combined shape"
+                      src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-47.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail-6">
+          <div className="text-wrapper">Enterprise - 2019</div>
+          <div className="text-wrapper-2">Project Name</div>
+          <div className="safari-wrapper">
+            <div className="safari">
+              <div className="overlap-group">
+                <img className="BG" alt="Bg" src="https://c.animaapp.com/GMqBQNDG/img/bg-10.svg" />
+                <div className="interface">
+                  <div className="adress">
+                    <div className="overlap-group-9">
+                      <img className="page" alt="Page" src="https://c.animaapp.com/GMqBQNDG/img/page-1-5.svg" />
+                      <div className="domain">website.co</div>
+                    </div>
+                  </div>
+                  <div className="new-page">
+                    <img className="icon" alt="Icon" src="https://c.animaapp.com/GMqBQNDG/img/icon-5.svg" />
+                  </div>
+                  <div className="sites-5">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-3">
+                        <img
+                          className="combined-shape"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-42.svg"
+                        />
+                        <img
+                          className="img"
+                          alt="Combined shape"
+                          src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-43.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="share-6">
+                    <div className="div-wrapper">
+                      <div className="overlap-group-4">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                      </div>
+                    </div>
+                  </div>
+                  <img className="dots" alt="Dots" src="https://c.animaapp.com/GMqBQNDG/img/dots-5@2x.png" />
+                  <img className="arrows" alt="Arrows" src="https://c.animaapp.com/GMqBQNDG/img/arrows-5@2x.png" />
+                  <div className="tabs-4">
+                    <img
+                      className="combined-shape-2"
+                      alt="Combined shape"
+                      src="https://c.animaapp.com/GMqBQNDG/img/combined-shape-47.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="intro">
+          <p className="i-m-a-product">
+            I&#39;m a Product Designer with UI &amp; UX skills
+            <br />
+            who love to build powerful and elegant products
+            <br />
+            with x years of experience in the tech industry
+            <br />
+            I&#39;m currently working at Lorem Ipsum.
+          </p>
+          <img className="hello" alt="Hello" src="https://c.animaapp.com/GMqBQNDG/img/hello@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
